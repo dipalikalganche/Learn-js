0af392c9cc5230770378135a186b665862891b5f
dipalikalganche <dipalikalganche@gmail.com>
2025-12-23 23:58:17 +0530

    add day4 code

---
+++ lec4/index.js
@@ -0,0 +1,100 @@
+// Operator
+// Arithmetic
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(6/2);
+// console.log(5%2);
+// // Reminder
+// console.log(5**3);//it also known as cube of 5
+
+// Assignment operator
+
+// let x = 20;
+// let y = 10;
+// x = x+y;
+// x = x-y;
+// x = x*y;
+// x+=y;
+// x = x+y;
+// x%=y;
+// console.log(x);
+
+// comparison operator
+// let x = 20;
+// let y= 10;
+
+// console.log(x>y);
+// console.log(x>=y);
+// console.log(x<y);
+// console.log(x<=y);
+// console.log(x==y);
+// console.log(x===y);
+
+// type check(both of  same type ===, then comparison)
+
+// number and string compare(string convert number)
+// String--> Number
+// let z = "120";
+// let a = Number(z);
+
+// console.log(a);
+// NaN: Not a number(Type of it is number)
+
+// let a = "121ac";
+// let b = Number(a);
+// console.log(typeof b);
+
+// let a = true;
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number(null));
+// console.log(Number(undefined));
+
+// null--> 0
+//  undefined-->NaN
+
+// Number -> String
+let a = 10;
+let b = String(a);
+console.log(typeof b);
+// "true"
+// true
+
+// console.log(typeof String(undefined));
+
+// Boolean conversion
+
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+
+// let a = 0.1;
+// let b = 0.2;
+// let c = a+b;
+// console.log(c);
+
+// 1: null is loosely equal to undefined only
+
+// console.log(null==undefined);
+// console.log(null===undefined);
+// console.log(null==0);
+// console.log(null=="");
+// console.log(null==false);
+// console.log(null==true);
+
+// >,<,>=,<= (null --> number, undefined --> NaN)
+
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=undefined);
+// console.log(undefined>=0);
+
+// console.log("Rohit">"Rahit");
+// console.log(10<true);
+// console.log(null>="");
+// console.log(NaN == NaN);
